3a61407edf1805aac10db356d5f32b2a2175d118
Jacob Smith <jacob101607@yahoo.com>
2015-12-21 18:50:39 -0600

    Manage data model classes with lombok.

--- src/main/java/gmapi/models/DeviceInfo.java
+++ src/main/java/gmapi/models/DeviceInfo.java
@@ -1,5 +1,8 @@
 package gmapi.models;
 
+import lombok.Data;
+
+@Data
 public class DeviceInfo {
 
 	private String id;
@@ -8,48 +11,10 @@ public class DeviceInfo {
 	private long lastAccessTimeMs;
 	
 	public DeviceInfo( ) {
-		this.id = "Unknown";
-		this.friendlyName = "Unknown";
-		this.type = "Unknown";
+		this.id               = "Unknown";
+		this.friendlyName     = "Unknown";
+		this.type             = "Unknown";
 		this.lastAccessTimeMs = 0;
-	}
-
-	public String getId( ) {
-		return id;
-	}
-
-	public void setId( String id ) {
-		this.id = id;
-	}
-
-	public String getFriendlyName( ) {
-		return friendlyName;
-	}
-
-	public void setFriendlyName( String friendlyName ) {
-		this.friendlyName = friendlyName;
-	}
-
-	public String getType( ) {
-		return type;
-	}
-
-	public void setType( String type ) {
-		this.type = type;
-	}
-
-	public long getLastAccessTimeMs( ) {
-		return lastAccessTimeMs;
-	}
-
-	public void setLastAccessTimeMs( long lastAccessTimeMs ) {
-		this.lastAccessTimeMs = lastAccessTimeMs;
-	}
-
-	@Override
-	public String toString( ) {
-		return "DeviceInfo [id=" + id + ", friendlyName=" + friendlyName + ", type=" + type + ", lastAccessTimeMs="
-				+ lastAccessTimeMs + "]";
 	}
 	
 }
--- src/main/java/gmapi/models/Track.java
+++ src/main/java/gmapi/models/Track.java
@@ -1,77 +1,54 @@
 package gmapi.models;
 
+import java.net.URL;
+
+import lombok.Data;
+
+@Data
 public class Track {
 
-	private String album;
-	private String title;
-	private String genre;
-	private String artist;
-	private String id;
+	private String kind;
 	
+	private String title;
+	private String artist;
+	private String album;
+	private String albumArtist;
+	
+	private String genre;
+	private String trackType;
+	
+	private URL[ ] albumArtRef;
+	private URL[ ] artistArtRef;
+	
+	private String storeId;
+	private String albumId;
+	private String[ ] artistId;
+	private String nid;
+	
+	private int year;
+	private int discNumber;
 	private int trackNumber;
 	
 	public Track( ) {
-		this.album  = "Unknown";
-		this.title  = "Unknown";
-		this.genre  = "Unknown";
-		this.artist = "Unknown";
-		this.id     = "Unknown";
+		this.kind         = "Unknown";
+		this.title        = "Unknown";
+		this.artist       = "Unknown";
+		this.albumArtist  = "Unknown";
 		
-		this.trackNumber = 0;
-	}
-
-	public String getAlbum( ) {
-		return album;
-	}
-
-	public void setAlbum( String album ) {
-		this.album = album;
-	}
-
-	public String getTitle( ) {
-		return title;
-	}
-
-	public void setTitle( String title ) {
-		this.title = title;
-	}
-
-	public String getGenre( ) {
-		return genre;
-	}
-
-	public void setGenre( String genre ) {
-		this.genre = genre;
-	}
-
-	public String getArtist( ) {
-		return artist;
-	}
-
-	public void setArtist( String artist ) {
-		this.artist = artist;
-	}
-
-	public String getId( ) {
-		return id;
-	}
-
-	public void setId( String id ) {
-		this.id = id;
-	}
-
-	public int getTrackNumber( ) {
-		return trackNumber;
-	}
-
-	public void setTrackNumber( int trackNumber ) {
-		this.trackNumber = trackNumber;
-	}
-
-	@Override
-	public String toString( ) {
-		return "Track [album=" + album + ", title=" + title + ", genre=" + genre + ", artist=" + artist + ", id=" + id
-				+ ", trackNumber=" + trackNumber + "]";
+		this.genre        = "Unknown";
+		this.trackType    = "Unknown";
+		
+		this.albumArtRef  = new URL[ 0 ];
+		this.artistArtRef = new URL[ 0 ];
+		
+		this.storeId      = "";
+		this.albumId      = "";
+		this.artistId     = new String[ 0 ];
+		this.nid          = "";
+		
+		this.year        = -1;
+		this.discNumber  = -1;
+		this.trackNumber = -1;
 	}
 
 }
